935593eae89969f05843fb6674f6661214f4767f
Omotola Macaulay <tolamacaulay@gmail.com>
2023-07-19 14:14:26 +0100

    Implement service worker

---
+++ public/serviceWorker.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = "news-app-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/static/js/0.chunk.js",
+  "/static/js/main.chunk.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        // eslint-disable-next-line array-callback-return
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { Workbox } from "workbox-window";
+// import reportWebVitals from "./reportWebVitals";
+// import { Workbox } from "workbox-window";
 // import * as serviceWorker from "../src/service-worker";
+// import * as serviceWorker from "./serviceWorker";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -14,25 +15,34 @@ root.render(
     <App />
   </React.StrictMode>
 );
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./serviceWorker.js")
+      .then((reg) => console.log("Service worker registered"))
+      .catch((err) => console.log("Service worker registration failed", err));
+  });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-if ("serviceWorker" in navigator) {
-  const wb = new Workbox("/service-worker.js");
+// reportWebVitals();
+// serviceWorker.register();
+// if ("serviceWorker" in navigator) {
+//   const wb = new Workbox("/service-worker.js");
 
-  wb.addEventListener("activated", (event) => {
-    if (!event.isUpdate) {
-      // Fresh service worker was installed
-      console.log("Service Worker activated for the first time");
-    } else {
-      // Service worker update was installed
-      console.log("Service Worker updated");
-    }
-  });
+//   wb.addEventListener("activated", (event) => {
+//     if (!event.isUpdate) {
+//       // Fresh service worker was installed
+//       console.log("Service Worker activated for the first time");
+//     } else {
+//       // Service worker update was installed
+//       console.log("Service Worker updated");
+//     }
+//   });
 
-  wb.register().catch((error) =>
-    console.log("Service Worker registration failed:", error)
-  );
-}
+//   wb.register().catch((error) =>
+//     console.log("Service Worker registration failed:", error)
+//   );
+// }
